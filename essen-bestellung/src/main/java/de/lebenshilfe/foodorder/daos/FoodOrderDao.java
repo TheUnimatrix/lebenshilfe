ffe924b246d43b23fe6e17f15c9547ad0c0fe76d
segi <segi@segi.wfbm-schleiz.local>
2022-03-21 15:16:32 +0100

    Klassen zum persistenten Speichern von Bestellungen hinzugefügt

---
+++ essen-bestellung/src/main/java/de/lebenshilfe/foodorder/daos/FoodOrderDao.java
@@ -0,0 +1,9 @@
+package de.lebenshilfe.foodorder.daos;
+
+import de.lebenshilfe.foodorder.models.FoodOrder;
+
+public class FoodOrderDao extends AbstractDao<FoodOrder> {
+	
+	
+	
+}
---
+++ essen-bestellung/src/main/java/de/lebenshilfe/foodorder/daos/OrderPositionDao.java
@@ -0,0 +1,7 @@
+package de.lebenshilfe.foodorder.daos;
+
+import de.lebenshilfe.foodorder.models.OrderPosition;
+
+public class OrderPositionDao extends AbstractDao<OrderPosition> {
+	
+}
