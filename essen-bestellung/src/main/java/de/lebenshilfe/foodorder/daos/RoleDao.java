d07e86123c1ae4048df9c3d2bda316681c27abeb
segi <segi@segi.wfbm-schleiz.local>
2022-03-22 13:54:18 +0100

    Klasse zum persistenten Speichern der Rollen hinzugefügt

---
+++ essen-bestellung/src/main/java/de/lebenshilfe/foodorder/daos/RoleDao.java
@@ -0,0 +1,31 @@
+package de.lebenshilfe.foodorder.daos;
+
+import de.lebenshilfe.foodorder.models.Role;
+
+public class RoleDao extends AbstractDao<Role> {
+	
+	public void saveOrUpdateRole(Role role) {
+		if (role != null) {
+			
+			Integer roleId = role.getId();
+//			System.out.println(addressId);
+			
+			if (roleId == null) {
+				super.saveObject(role);
+			} else {
+				super.updateObject(role);
+			}
+		}
+	}
+	
+	public Role getRoleById(Integer roleId) {
+		Role role = null;
+		
+		if (roleId != null) {
+			role = super.getObjectById(Role.class, roleId);
+		}
+		
+		return role;
+	}
+	
+}
