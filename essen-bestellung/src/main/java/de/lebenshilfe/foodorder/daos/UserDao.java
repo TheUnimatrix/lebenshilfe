7b6bfa42834b94ccc86b5f07e9562a47398d39ff
segi <segi@segi.wfbm-schleiz.local>
2022-03-23 14:52:03 +0100

    Speichern und Holen von Benutzern an Superklasse ausgelagert

--- essen-bestellung/src/main/java/de/lebenshilfe/foodorder/daos/UserDao.java
+++ essen-bestellung/src/main/java/de/lebenshilfe/foodorder/daos/UserDao.java
@@ -1,29 +1,67 @@
 package de.lebenshilfe.foodorder.daos;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import de.lebenshilfe.foodorder.models.User;
+import de.lebenshilfe.foodorder.models.User_;
 import de.lebenshilfe.foodorder.utils.HibernateUtils;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 
 public class UserDao extends AbstractDao<User> {
 	
 	public void saveOrUpdateUser(User user) {
 		if (user != null) {
-			super.updateObject(user);
+			
+			Integer userId = user.getId();
+			
+			if (userId == null) {
+				super.saveObject(user);
+			} else {
+				super.updateObject(user);
+			}
 		}
 	}
 	
 	public User getUserByUserId(Integer userId) {
+		User user = null;
+		
+		if (userId != null) {
+			user = super.getObjectById(User.class, userId);
+		}
+		
+		return user;
+	}
+	
+	public User getUserByEmail(String email) {
+		
+		if (email == null) {
+			return null;
+		}
+		
 		SessionFactory sessionFactory = HibernateUtils.getSessionFactory();
 		Session session = sessionFactory.openSession();
 		
-//		Transaction tx = session.beginTransaction();
-		User user = session.byId(User.class).load(userId);
+		User user = null;
 		
-//		tx.commit();
-		session.close();
+		CriteriaBuilder builder = session.getCriteriaBuilder();
+		CriteriaQuery<User> query = builder.createQuery(User.class);
 		
+		Root<User> root = query.from(User.class);
+		query.where(builder.equal(root.get(User_.email), email));
+		
+		List<User> users = session.createQuery(query).getResultList();
+		
+		// Wenn Benutzer gefunden wurde, soll dieser ausgelesen werden
+		if (users.size() > 0) {
+			user = users.get(0);
+		}
+		
+		// Gib den (gefundenen) Benutzer zurück
 		return user;
 	}
 	
