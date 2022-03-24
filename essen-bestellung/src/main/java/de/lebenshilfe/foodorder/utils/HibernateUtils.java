47ee301ef7282431bd0ef6fe9a9ac5302c03cae1
segi <segi@segi.wfbm-schleiz.local>
2022-03-24 15:56:10 +0100

    Passwörter und Preisgruppe bei Benutzern hinzugefügt

--- essen-bestellung/src/main/java/de/lebenshilfe/foodorder/utils/HibernateUtils.java
+++ essen-bestellung/src/main/java/de/lebenshilfe/foodorder/utils/HibernateUtils.java
@@ -63,43 +63,61 @@ public class HibernateUtils {
 		User c1 = new User();
 		c1.setName("Tagesstätte Schleiz");
 		c1.setEmail("ts-scz@example.com");
+		c1.setPassword(PasswordUtils.encodePassword("asd"));
 		c1.setDeliveryAddress(addr2);
 		c1.setBillingAddress(addr1);
 		c1.setRole(Role.USER);
+		c1.setPriceGroup(1);
 		
 		User c2 = new User();
 		c2.setName("Schleizer Werkstätten gGmbH");
 		c2.setEmail("wfbm-scz@example.com");
+		c2.setPassword(PasswordUtils.encodePassword("asd"));
 		c2.setDeliveryAddress(addr1);
 		c2.setBillingAddress(addr1);
 		c2.setRole(Role.USER);
+		c2.setPriceGroup(2);
 		
 		User c3 = new User();
 		c3.setName("Sebastian");
 		c3.setEmail("segi@example.com");
+		c3.setPassword(PasswordUtils.encodePassword("asd"));
 		c3.setDeliveryAddress(addr5);
 		c3.setBillingAddress(addr5);
 		c3.setRole(Role.ADMIN);
+		c3.setPriceGroup(15);
 		
 		User c4 = new User();
 		c4.setName("Frau Wetzel");
 		c4.setEmail("frwe@example.com");
+		c4.setPassword(PasswordUtils.encodePassword("asd"));
 		c4.setDeliveryAddress(addr1);
 		c4.setBillingAddress(addr1);
 		c4.setRole(Role.MANAGER);
+		c4.setPriceGroup(6);
+		
+		User c5 = new User();
+		c5.setName("René Poser");
+		c5.setEmail("repo@example.com");
+		c5.setPassword(PasswordUtils.encodePassword("asd"));
+		c5.setDeliveryAddress(addr1);
+		c5.setBillingAddress(addr1);
+		c5.setRole(Role.ADMIN);
+		c5.setPriceGroup(6);
 		
 		AddressDao addrDao = new AddressDao();
-		addrDao.saveOrUpdateAddress(addr1);
 		addrDao.saveOrUpdateAddress(addr1);
 		addrDao.saveOrUpdateAddress(addr2);
 		addrDao.saveOrUpdateAddress(addr3);
 		addrDao.saveOrUpdateAddress(addr4);
+		addrDao.saveOrUpdateAddress(addr5);
 		
 		UserDao cDao = new UserDao();
 		cDao.saveOrUpdateUser(c1);
 		cDao.saveOrUpdateUser(c2);
 		cDao.saveOrUpdateUser(c3);
 		cDao.saveOrUpdateUser(c4);
+		cDao.saveOrUpdateUser(c5);
 	}
 
 	public static SessionFactory getSessionFactory() {
