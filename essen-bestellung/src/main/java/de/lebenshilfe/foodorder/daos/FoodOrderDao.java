d9eadfe4dc1a00d3c83b5fbbd43785e021cc617b
segi <segi@segi.wfbm-schleiz.local>
2022-03-24 15:52:49 +0100

    Methode zum Abspeichern einer Bestellung hinzugefügt

--- essen-bestellung/src/main/java/de/lebenshilfe/foodorder/daos/FoodOrderDao.java
+++ essen-bestellung/src/main/java/de/lebenshilfe/foodorder/daos/FoodOrderDao.java
@@ -4,6 +4,17 @@ import de.lebenshilfe.foodorder.models.FoodOrder;
 
 public class FoodOrderDao extends AbstractDao<FoodOrder> {
 	
-	
+	public void saveOrUpdateFoodOrder(FoodOrder foodOrder) {
+		if (foodOrder != null) {
+			
+			Integer foodOrderId = foodOrder.getId();
+			
+			if (foodOrderId == null) {
+				super.saveObject(foodOrder);
+			} else {
+				super.updateObject(foodOrder);
+			}
+		}
+	}
 	
 }
